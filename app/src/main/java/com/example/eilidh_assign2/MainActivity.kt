2e518af8069fbabd5e1725c1d1224a359f8344a7
sneli <emacsymic@gmail.com>
2025-02-23 03:00:30 -0700

    Finished Assignment

--- app/src/main/java/com/example/eilidh_assign2/MainActivity.kt
+++ app/src/main/java/com/example/eilidh_assign2/MainActivity.kt
@@ -10,7 +10,6 @@
 package com.example.eilidh_assign2
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -41,11 +40,9 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.eilidh_assign2.data.Datasource
-import com.example.eilidh_assign2.model.ArtSpace
 import com.example.eilidh_assign2.ui.theme.Eilidh_assign2Theme
 
 class MainActivity : ComponentActivity() {
